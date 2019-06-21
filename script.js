d3a6e59f1d724e5ffe9c714e66760329716aaf2c
Ivan Filippov <ivan_filippov@outlook.com>
2019-06-21 08:43:11 +0300

    domashka 3

--- script.js
+++ script.js
@@ -2,56 +2,155 @@
 
 let searchForm = document.querySelector('#search-form');
 let movies = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w200/';
+const api_key = '058af9bcc056b256f8a414cbc87d4523';
+
+const apiFetch = (request, callback) => {
+    fetch(request)
+        .then((value) => {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
+            return value.json();
+        })
+        .then((output) => {
+            if (typeof callback === 'function') {
+                callback(output);
+            }
+        })
+        .catch((reason) => {
+            movies.innerHTML = 'Упс, что-то пошло не так!';
+            console.log(reason || reason.status);
+        });
+}
+
+const addEventMedia = () => {
+    let media = movies.querySelectorAll('img[data-id]');
+    media.forEach((elem) => {
+        elem.style.cursor = 'pointer';
+        elem.addEventListener('click', showFullInfo);
+    });
+}
 
 const apiSearch = (event) => {
     event.preventDefault();
 
     let searchText = document.querySelector('.form-control').value;
-    if (searchText.length == 0) {
-        movies.innerHTML = 'Введите название!';
+    if (searchText.trim().length === 0) {
+        movies.innerHTML = '<h2 class="col-12 text-center text-danger">Поле поиска не должно быть пустым</h2>';
         return;
     }
-    let server = 'https://api.themoviedb.org/3/search/multi?api_key=058af9bcc056b256f8a414cbc87d4523&language=ru&query=' + searchText;
-    movies.innerHTML = 'Загрузка...';
+    let server = `https://api.themoviedb.org/3/search/multi?api_key=${api_key}&language=ru&query=` + searchText;
+    movies.innerHTML = '<div class="spinner"></div>';
 
-    fetch(server)
-        .then(function(value) {
-            if (value.status !== 200) {
-                return Promise.reject();
-            }
-            return value.json();
-        })
-        .then(function(output) {
-            let inner = '';
+    apiFetch(server, (output) => {
 
-            if (output.results.length == 0) {
-                inner = 'Фильмы не найдены';
-            } else {
-                output.results.forEach((item) => {
+        let inner = '';
+        if (output.results.length === 0) {
+            inner = '<h2 class="col-12 text-center text-info">По запросу ничего не найдено</h2>';
+        } else {
+            output.results.forEach((item) => {
 
-                    let nameItem = item.name || item.title;
-                    let dateItem = item.first_air_date || item.release_date || 'нет данных';
-                    let posterItem = item.poster_path ? `https://image.tmdb.org/t/p/w200/${item.poster_path}` : 'https://placekitten.com/200/300';
+                let nameItem = item.name || item.title;
+                let dateItem = item.first_air_date || item.release_date || 'нет данных';
+                let posterItem = item.poster_path ? `https://image.tmdb.org/t/p/w200/${item.poster_path}` : './img/noposter.jpg';
 
-                    inner += `<a href="https://www.themoviedb.org/${item.media_type}/${item.id}" class="item col-12 col-sm-6 col-md-4 col-lg-3 text-center" title="${item.overview}" target="_blank">
-                        <img src="${posterItem}"> <br/>
-                        <b> ${nameItem} </b> <span class="badge badge-primary"> ${item.vote_average} </span> <br/>
-                        Релиз: ${dateItem}
-                    </a>`;
+                let dataInfo = '';
+                if (item.media_type !== 'person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
 
-                });
-            }
+                inner += `<div class="item col-12 col-sm-6 col-md-4 col-lg-3 text-center">
+                    <img src="${posterItem}" class="img_poster" alt="${nameItem}" ${dataInfo}> <br/>
+                    <b> ${nameItem} </b> <span class="badge badge-primary"> ${item.vote_average} </span> <br/>
+                    Релиз: ${dateItem}
+                </div>`;
 
-            movies.innerHTML = inner;
-        })
-        .catch(function(reason) {
-            movies.innerHTML = 'Упс, что-то пошло не так!';
-            console.log('error: ' + reason.status);
-        })
+            });
+        }
 
+        movies.innerHTML = inner;
+        addEventMedia();
+
+    });
 }
 
-searchForm.addEventListener('submit', apiSearch);
-searchForm.addEventListener('change', apiSearch);
 
+function showFullInfo() {
+    let url = '';
+    if (this.dataset.type === 'movie') {
+        url = `https://api.themoviedb.org/3/movie/${this.dataset.id}?api_key=${api_key}&language=ru`;
+    } else if (this.dataset.type === 'tv') {
+        url = `https://api.themoviedb.org/3/tv/${this.dataset.id}?api_key=${api_key}&language=ru`;
+    } else {
+        movies.innerHTML = '<h2 class="col-12 text-center text-info">Ошибка. Повторите позже.</h2>';
+    }
 
+    apiFetch(url, (output) => {
+
+        // console.log(output);
+
+        let genres = '';
+        output.genres.forEach((g) => {
+            genres += g.name + ', ';
+        });
+        genres = genres.slice(0, -2);
+
+        movies.innerHTML = `
+        <h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+        <div class="col-4">
+            <img src="${urlPoster + output.poster_path}" alt="${output.name || output.title}" >
+        </div>
+        <div class="col-8">
+        
+        ${(output.homepage) ? `<p><a href="${output.homepage}" target="_blank">Официальная страница</a></p>` : ''}
+        ${(output.imdb_id) ? `<p><a href="https://imdb.com/title/${output.imdb_id}" target="_blank">Ссылка на IMDB</a></p>` : ''}
+        
+        <p>Рейтинг: ${output.vote_average}</p>
+        <p>Жанр: ${genres} </p>
+        <p>Статус: ${output.status}</p>
+        <p>Премьера: ${output.first_air_date || output.release_date }</p>
+        
+        ${(output.last_episode_to_air) ? `<p>${output.number_of_seasons} сезон ${output.last_episode_to_air.episode_number} серий вышло</p>` : ''}
+        
+        <p> Описание: ${output.overview}</p>
+        
+        </div>
+        `;
+
+    });
+}
+
+const showTopWeek = () => {
+
+    apiFetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}&language=ru`, (output) => {
+
+        let inner = '<h4 class="col-12 text-center text-info">Популярные за неделю</h4>';
+        if (output.results.length === 0) {
+            inner = '<h2 class="col-12 text-center text-info">По запросу ничего не найдено</h2>';
+        } else {
+            output.results.forEach((item) => {
+                let nameItem = item.name || item.title;
+                let mediaType = item.title ? 'movie' : 'tv';
+                let dateItem = item.first_air_date || item.release_date || 'нет данных';
+                let posterItem = item.poster_path ? `https://image.tmdb.org/t/p/w200/${item.poster_path}` : './img/noposter.jpg';
+                let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+                inner += `<div class="item col-12 col-sm-6 col-md-4 col-lg-3 text-center">
+                    <img src="${posterItem}" class="img_poster" alt="${nameItem}" ${dataInfo}> <br/>
+                    <b> ${nameItem} </b> <span class="badge badge-primary"> ${item.vote_average} </span> <br/>
+                    Релиз: ${dateItem}
+                </div>`;
+            });
+        }
+        movies.innerHTML = inner;
+        addEventMedia();
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    searchForm.addEventListener('submit', apiSearch);
+    searchForm.addEventListener('change', apiSearch);
+
+    showTopWeek();
+
+});
